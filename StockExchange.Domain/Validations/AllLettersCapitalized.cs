6ccfdbeec76254e7aa1df13dcae8fa4314259316
rucen <ucen.rich@gmail.com>
2025-11-06 15:10:17 +0100

    Add client validation - TickerSymbol has to be capitalized

--- StockExchange.Domain/Validations/AllLettersCapitalized.cs
+++ StockExchange.Domain/Validations/AllLettersCapitalized.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace StockExchange.Domain.Validations
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
-    internal class AllLettersCapitalizedAttribute : ValidationAttribute
+    internal class AllLettersCapitalizedAttribute : ValidationAttribute, IClientModelValidator
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
@@ -37,6 +38,16 @@ namespace StockExchange.Domain.Validations
                 throw new NotImplementedException($"The {nameof(AllLettersCapitalizedAttribute)} is not implemented for the type: {value.GetType()}");
             }
 
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (context.Attributes.ContainsKey("data-val") == false)
+            {
+                context.Attributes.Add("data-val", "true");
+            }
+
+            context.Attributes.Add("data-val-allletterscapitalized", "All letters must be capitalized.");
         }
     }
 }
---
+++ StockExchange.Web/wwwroot/js/Validations/AllLettersCapitalized.js
@@ -0,0 +1,20 @@
+﻿$.validator.addMethod('allletterscapitalized',
+    function (value, element, params) {
+        if (!value || value == '')
+            return true;
+
+        if (value.toUpperCase() == value) {
+            return true;
+        }
+
+        return false;
+    });
+
+
+$.validator.unobtrusive.adapters.add('allletterscapitalized', '',
+    function (options) {
+        var element = $(options.form).find('#Description')[0];
+
+        options.rules['allletterscapitalized'] = [element, ''];
+        options.messages['allletterscapitalized'] = options.message;
+    });
