8ed5b5c2a098c93661d9025598069a4291a0f714
rucen <ucen.rich@gmail.com>
2025-11-16 15:59:28 +0100

    Reset database & populate with new clean data

---
+++ StockExchange.Infrastructure/Migrations/20251116145127_ChangeOrderAndTransactionTypeValueToBUYFromBuy.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockExchange.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOrderAndTransactionTypeValueToBUYFromBuy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderType",
+                value: "BUY");
+
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderType",
+                value: "BUY");
+
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderType",
+                value: "BUY");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TransactionType",
+                value: "BUY");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "TransactionType",
+                value: "BUY");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "TransactionType",
+                value: "BUY");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderType",
+                value: "Buy");
+
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderType",
+                value: "Buy");
+
+            migrationBuilder.UpdateData(
+                table: "Order",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderType",
+                value: "Buy");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TransactionType",
+                value: "Buy");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "TransactionType",
+                value: "Buy");
+
+            migrationBuilder.UpdateData(
+                table: "Transaction",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "TransactionType",
+                value: "Buy");
+        }
+    }
+}
