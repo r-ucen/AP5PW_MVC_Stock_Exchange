0db7a719a4ed9b618b52c4cd087ba126d7ac2f67
rucen <ucen.rich@gmail.com>
2025-12-15 15:11:07 +0100

    Add dark/light theme switcher to settings

---
+++ StockExchange.Web/Controllers/SettingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StockExchange.Infrastructure.Identity.Enums;
+
+namespace StockExchange.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Select()
+        {
+            return View();
+        }
+    }
+}
